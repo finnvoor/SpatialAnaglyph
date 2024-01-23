79ff842bb47340936af8a428c1e357d66b1e5229
Finn Voorhees <finnvoorhees@gmail.com>
2024-01-23 09:34:39 +0000

    Add product

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 let package = Package(
     name: "SpatialAnaglyph",
     platforms: [.macOS(.v14)],
+    products: [
+        .executable(name: "SpatialAnaglyph", targets: ["SpatialAnaglyph"])
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0")
     ],
